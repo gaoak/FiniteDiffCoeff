6b9d39f2031c0a8d334ba90733eff4a31979556a
ankang <ankang@ubuntu.com>
2021-01-19 04:32:56 +0000

    update

--- main.cpp
+++ main.cpp
@@ -2,14 +2,49 @@
 #include<vector>
 #include "reconstruction.h"
 using namespace std;
-int main() {
-    int k=3;
-    vector<double> coeff(k);
-    for(int r=0;r<3;++r) {
+int testcoefficient() {
+    vector<double> coeff(10);
+    int k;
+    printf("=====================\n");
+    k=1;
+    for(int r=-1;r<k;++r) {
         recon_coef_u_(&r, &k, coeff.data());
         for(int j=0;j<k;++j) {
             printf("%f,",coeff[j]);
         }
         printf("\n");
     }
+    printf("=====================\n");
+    k=2;
+    for(int r=-1;r<k;++r) {
+        recon_coef_u_(&r, &k, coeff.data());
+        for(int j=0;j<k;++j) {
+            printf("%f,",coeff[j]);
+        }
+        printf("\n");
+    }
+    printf("=====================\n");
+    k=3;
+    for(int r=-1;r<k;++r) {
+        recon_coef_u_(&r, &k, coeff.data());
+        for(int j=0;j<k;++j) {
+            printf("%f,",coeff[j]);
+        }
+        printf("\n");
+    }
+    printf("=====================\n");
+    k=4;
+    for(int r=-1;r<k;++r) {
+        recon_coef_u_(&r, &k, coeff.data());
+        for(int j=0;j<k;++j) {
+            printf("%f,",coeff[j]);
+        }
+        printf("\n");
+    }
+    printf("=====================\n");
+    return 0;
+}
+
+int main() {
+    ;
 }
--- reconstruction.h
+++ reconstruction.h
@@ -2,3 +2,4 @@
 // integer    int*
 // integer array, int *
 extern "C" void recon_coef_u_(int *r, int *k, double *coeff);
+extern "C" void recon_coef_a_(int *r, int *k,double * coef, double *stencil);
